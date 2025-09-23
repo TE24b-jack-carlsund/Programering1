7cba8af9fc3ea837d02ff5917cb630a4ede012db
TE24b-jack-carlsund <jack.carlsund@elev.ga.ntig.se>
2025-09-23 13:48:21 +0200

    Interaktiv berättelse

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,119 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Skriv användarnamn!");
+string spelare1 = Console.ReadLine();
+string spelare2 = Console.ReadLine();
+string startspelare = spelare1;
+int hp2 = 100;
+int hp1 = 100;
+Console.WriteLine("Hej, välkomen till slagssmålspelet " + spelare1 + " och " + spelare2);
+Console.WriteLine("Ni har 100hp, den som besegrar den andra först vinner");
+int i = Random.Shared.Next(1, 3);
+if (i == 2)
+{
+    startspelare = spelare2;
+
+}
+Console.WriteLine("Spelare " + startspelare + " tryck i för att kasta tärningen");
+Console.WriteLine("");
+string aktivSpelare = startspelare;
+string motstandaren = spelare2;
+if (aktivSpelare == spelare2)
+{
+     motstandaren = spelare1;
+}
+
+while (hp1 > 0 && hp2 > 0)
+{
+    i = Random.Shared.Next(1, 4);
+    if (i == 1)
+    {
+        Console.WriteLine("Det händer inget");
+    }
+    else if (i == 2)
+    {
+        Console.WriteLine(aktivSpelare + " får sparka " + motstandaren);
+        if (aktivSpelare == spelare1)
+        {
+            hp2 = hp2 - 30;
+            Console.WriteLine(motstandaren + " har " + Math.Max(0,hp2) + " kvar.");
+        }
+        else if (aktivSpelare == spelare2)
+        {
+            hp1 = hp1 - 30;
+            Console.WriteLine(motstandaren + " har " + Math.Max(0,hp1) + " kvar.");
+        }
+
+
+    }
+    else if (i == 3)
+    {
+
+        Console.WriteLine(aktivSpelare + " får slå " + motstandaren);
+        if (aktivSpelare == spelare1)
+        {
+            hp2 = hp2 - 20;
+            Console.WriteLine(motstandaren + " har " + Math.Max(0,hp2) + " kvar.");
+        }
+        else if (aktivSpelare == spelare2)
+        {
+            hp1 = hp1 - 20;
+            Console.WriteLine(motstandaren + " har " + Math.Max(0,hp1) + " kvar.");
+        }
+    }
+    if (hp1 > 0 && hp2 > 0)
+    {
+       Console.WriteLine("Nu är det " + motstandaren + " tur att slå.");
+    }
+    if (aktivSpelare == spelare1)
+        {
+            aktivSpelare = spelare2;
+            motstandaren = spelare1;
+        }
+        else
+        {
+            aktivSpelare = spelare1;
+            motstandaren = spelare2;
+        }
+Console.WriteLine("");
+}
+Console.WriteLine("");
+Console.WriteLine(motstandaren + " har vunnit!");
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Random gen = new();
+// int i = Random.Shared.Next(10, 20);
+// Console.WriteLine(i);
+// Console.ReadLine();
+
+// int i = 100;
+// string name = "jack";
+// while (i > 0 && name == "jack")
+// {
+//     Console.WriteLine("Hello");
+//     i--;
+// }
+
+// int hitpoints = 100 + 50;
+// hitpoints = hitpoints - 10;
+
+// hitpoints -= 5;
+// hitpoints--;
+// hitpoints++;
+// string name = "Jack";
+
+// Console.WriteLine($"HP: {hitpoints}");
+// Console.ReadLine();
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
+Console.WriteLine("Hello and welcome to our quiz");
+Console.WriteLine("Chose between a to c");
+Console.WriteLine("1: Which city is in Japan?");
+Console.WriteLine("a) kagoshima b) stockholm c) New york");
+
+string a = "a";
+string b = "b";
+string c = "c";
+int point = 0;
+string player = Console.ReadLine();
+if (player == a)
+{
+    Console.WriteLine("All right");
+    point += 1;
+
+
+
+}
+else if (player == b)
+{
+    Console.WriteLine("Bad");
+    point -= 1;
+
+}
+else if (player == c)
+{
+    Console.WriteLine("Bad");
+    point -= 1;
+}
+
+Console.WriteLine("Your point is " + point);
+
+Console.WriteLine("2: Which one of these is an anime series?");
+Console.WriteLine("a) Naruto b) ruto c) Nato");
+a = "a";
+b = "b";
+c = "c";
+player = Console.ReadLine();
+
+if (player == a)
+{
+    Console.WriteLine("All right");
+   point += 1;
+}
+else if (player == b)
+{
+    Console.WriteLine("Bad");
+    point -= 1;
+
+}
+else if (player == c)
+{
+    Console.WriteLine("Bad");
+      point -= 1;
+}
+Console.WriteLine("Your point is " + point);
+Console.WriteLine("3: Which one of these is a video game");
+Console.WriteLine("a) Youtube b) Minecraft c) NetFlix");
+a = "a";
+b = "b";
+c = "c";
+player = Console.ReadLine();
+
+if (player == a)
+{
+    Console.WriteLine("Bad");
+    point -= 1;
+}
+else if (player == b)
+{
+    Console.WriteLine("All right");
+     point += 1;
+}
+else if (player == c)
+{
+    Console.WriteLine("Bad");
+      point -= 1;
+}
+
+Console.WriteLine("Your total is " + point);
+int total = point;
+if (total <= 1)
+{
+    Console.WriteLine("Your super bad");
+}
+Console.ReadLine();
+
